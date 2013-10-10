0d1cb16511e486bba0c36450835d05ca7a74d652
louis <louis@systemli.org>
2013-10-10 21:44:24 +0200

    add a deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+REMOTE_USER="www06"
+REMOTE_HOST="www-xeon.systemli.org"
+REMOTE_PATH="/var/www/systemli.org/www"
+REMOTE_PORT="2222"
+
+jekyll build
+rsync -avz --delete -e "ssh -p$REMOTE_PORT" _site/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
