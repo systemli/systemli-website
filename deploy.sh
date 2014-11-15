ef0850d49e9316c7d8d5ce5f5216154857c2d5e7
shadow <shadow@systemli.org>
2014-11-15 17:37:54 +0100

    [deploy.sh] adjusted for www-vmserv

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 REMOTE_USER="systemli"
-REMOTE_HOST="www-xeon.systemli.org"
+REMOTE_HOST="www-vmserv.systemli.org"
+#REMOTE_HOST="www-xeon.systemli.org"
 REMOTE_PATH="/var/www/www.systemli.org/www"
-REMOTE_PORT="2222"
+REMOTE_PORT="22"
+#REMOTE_PORT="2222"
 
 jekyll build
 rsync -avz --delete -e "ssh -p$REMOTE_PORT" _site/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
