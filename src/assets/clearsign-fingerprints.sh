cb375bf4fc474f8b2ae92a3ecfb953fc8e200fcd
doobry <doobry@systemli.org>
2016-12-21 00:13:53 +0100

    added current date to assets/fingerprints.txt
    * assets/clearsign-fingerprints.sh now updates the date in
      fingerprints.txt to today before signing the file.

--- src/assets/clearsign-fingerprints.sh
+++ src/assets/clearsign-fingerprints.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
+
+# update date in fingerprints.txt to today
+sed -i -e "s/^\(Date of last update: \)[0-9-]\+$/\1$(date +%F)/" fingerprints.txt
+
+# gpg-sign finterprints.txt
 gpg -u admin@systemli.org --clearsign fingerprints.txt
