95912c3851a994356eeef657e8f103fb4f38e28b
doobry <doobry@systemli.org>
2016-12-21 00:24:59 +0100

    make clearsign-fingerprints.sh path-safe
    * change paths in clearsign-fingerprints.sh script so it can be invoked
      from everywhere.
    * invoke clearsign-fingerprints.sh from deploy.sh if $UPDATE_FPS is set.

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,11 @@ REMOTE_PATH="/var/www/www.systemli.org/www"
 REMOTE_PORT="22"
 REMOTE_PORT2="2222"
 
+UPDATE_FPS="no"
+
 jekyll build
+if [ "$UPDATE_FPS" = "yes" ]; then
+	./src/assets/clearsign-fingerprints.sh
+fi
 rsync -avz --delete -e "ssh -p$REMOTE_PORT" _site/ $REMOTE_USER@$REMOTE_HOST:$REMOTE_PATH
 rsync -avz --delete -e "ssh -p$REMOTE_PORT2" _site/ $REMOTE_USER@$REMOTE_HOST2:$REMOTE_PATH
