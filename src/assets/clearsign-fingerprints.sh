f9633c2758c795ffd3d7f282c6eb1c6c5b2c565c
casper <casper@systemli.org>
2016-12-23 16:10:35 +0100

    Update signing and upload procedure

--- src/assets/clearsign-fingerprints.sh
+++ src/assets/clearsign-fingerprints.sh
@@ -6,5 +6,8 @@ basedir="$(dirname $0)"
 # update date in fingerprints.txt to today
 sed -i -e "s/^\(Date of last update: \)[0-9-]\+$/\1$(date +%F)/" $basedir/fingerprints.txt
 
+# remove old fingerprints
+rm $basedir/fingerprint.txt.asc
+
 # gpg-sign finterprints.txt
 gpg -u admin@systemli.org --clearsign $basedir/fingerprints.txt
