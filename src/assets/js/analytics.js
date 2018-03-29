6984188b3a41e42092416203c60f9a379be67c03
casper <casper@systemli.org>
2018-03-29 12:07:57 +0200

    Load matomo code via script src

---
+++ src/assets/js/analytics.js
@@ -0,0 +1,8 @@
+var idSite = 2;
+var piwikTrackingApiUrl = 'https://analytics.systemli.org/piwik.php';
+
+var _paq = _paq || [];  
+_paq.push(['setTrackerUrl', piwikTrackingApiUrl]);
+_paq.push(['setSiteId', idSite]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);  
