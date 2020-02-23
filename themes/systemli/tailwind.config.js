106e4f6b00e832edca6d00ff55fd83139082933b
batabana <batabana-github@systemli.org>
2020-02-23 15:17:33 +0100

    move to tailwind css

---
+++ themes/systemli/tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      'body': ['"TSTAR TW PRO"' , 'sans-serif'],
+    },
+    colors: {
+      lightblue: '#42abee',
+      pink: '#ff52ad',
+      lightgrey: '#f5f5f5',
+      white: '#fff',
+      black: '#000',
+    },
+    fontSize: {
+      'xs': '12px',
+      's': '16px',
+      'm': '17px',
+      'l': '23px',
+    }
+  },
+  variants: {},
+  plugins: [],
+}
