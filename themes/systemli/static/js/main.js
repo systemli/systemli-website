29ffbf1f654bb6f1bbcecc77e3a797c0903b88fd
batabana <batabana-github@systemli.org>
2020-05-03 16:54:56 +0200

    mobile menu height

--- themes/systemli/static/js/main.js
+++ themes/systemli/static/js/main.js
@@ -1,13 +1,9 @@
 (function () {
     // show and hide menu
     document.getElementById('button-menu-open').addEventListener('click', function () {
-        document.getElementById('navbar').classList.replace('w-0', 'w-screen');
-        document.getElementById('navbar').classList.replace('pl-0', 'pl-40');
         document.getElementById('navbar').classList.replace('hidden', 'flex');
     });
     document.getElementById('button-menu-close').addEventListener('click', function () {
-        document.getElementById('navbar').classList.replace('w-screen', 'w-0');
-        document.getElementById('navbar').classList.replace('pl-40', 'pl-0');
         document.getElementById('navbar').classList.replace('flex', 'hidden');
     });
     // show service description on hover
