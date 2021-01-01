380798c0a6bf223adedcf8f884967e97de4c8afc
louis <louis@systemli.org>
2021-01-01 16:32:26 +0100

    disable cookies and respect DNT for matomo

--- themes/systemli/assets/js/matomo.js
+++ themes/systemli/assets/js/matomo.js
@@ -1,9 +1,17 @@
-var idSite = 2;
-var piwikTrackingApiUrl = 'https://analytics.systemli.org/piwik.php';
-
-var _paq = _paq || [];
-_paq.push(['setTrackerUrl', piwikTrackingApiUrl]);
-_paq.push(['setSiteId', idSite]);
-_paq.push(['disableCookies']);
-_paq.push(['trackPageView']);
+var _paq = window._paq = window._paq || []
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['setCookieDomain', '*.systemli.org'])
+_paq.push(['setDoNotTrack', true])
+_paq.push(['disableCookies'])
+_paq.push(['trackPageView'])
 _paq.push(['enableLinkTracking']);
+(function () {
+  var u = 'https://analytics.systemli.org/'
+  _paq.push(['setTrackerUrl', u + 'matomo.php'])
+  _paq.push(['setSiteId', '2'])
+  var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0]
+  g.type = 'text/javascript'
+  g.async = true
+  g.src = u + 'matomo.js'
+  s.parentNode.insertBefore(g, s)
+})()
