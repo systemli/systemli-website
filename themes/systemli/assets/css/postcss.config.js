e599c5a3360480bbe70e2720c019121b26713239
louis <louis@systemli.org>
2025-02-21 22:25:24 +0100

    Upgrade postcss-purgecss

--- themes/systemli/assets/css/postcss.config.js
+++ themes/systemli/assets/css/postcss.config.js
@@ -1,36 +1,37 @@
-const themeDir = 'themes/systemli/';
+const purgecss = require("@fullhuman/postcss-purgecss");
+const postcssImport = require("postcss-import");
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
 
-const purgecss = require('@fullhuman/postcss-purgecss')({
-
-  // Specify the paths to all of the template files in your project
-  content: [
-    themeDir + 'layouts/**/*.html',
-    'content/**/*.html',
-    'content/**/*.md',
-    'public/**/*.html',
-  ],
-
-  // This is the function used to extract class names from your templates
-  defaultExtractor: content => {
-    // Capture as liberally as possible, including things like `h-(screen-1.5)`
-    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || []
-
-    // Capture classes within other delimiters like .block(class="w-1/2") in Pug
-    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || []
-
-    return broadMatches.concat(innerMatches)
-  }
-})
+const themeDir = "themes/systemli/";
 
 module.exports = {
-  plugins: [
-    require('postcss-import')({
-      path: [themeDir]
-    }),
-    require('tailwindcss')(themeDir + 'assets/css/tailwind.config.js'),
-    require('autoprefixer')({
-      path: [themeDir]
-    }),
-    ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
-  ]
-}
+    plugins: [
+        postcssImport({
+            path: [themeDir],
+        }),
+        tailwindcss(themeDir + "assets/css/tailwind.config.js"),
+        autoprefixer({
+            path: [themeDir],
+        }),
+        purgecss.purgeCSSPlugin({
+            content: [
+                themeDir + "layouts/**/*.html",
+                "content/**/*.html",
+                "content/**/*.md",
+                "public/**/*.html",
+            ],
+            defaultExtractor: (content) => {
+                // Capture as liberally as possible, including things like `h-(screen-1.5)`
+                const broadMatches =
+                    content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+                // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+                const innerMatches =
+                    content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+                return broadMatches.concat(innerMatches);
+            },
+        }),
+    ],
+};
