150bd4c0e8158e45c525f6cacbc2dbdd650df369
louis <louis@systemli.org>
2025-02-21 22:06:11 +0100

    Upgrade to tailwindcss 2.2.16

--- themes/systemli/assets/css/tailwind.config.js
+++ themes/systemli/assets/css/tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     fontFamily: {
       'body': ['"TSTAR TW PRO"' , 'sans-serif'],
     },
