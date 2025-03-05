98dc5e512fa75bf366a75982571c62733b702a11
louis <louis@systemli.org>
2025-03-05 21:16:34 +0100

    Apply prettier

--- themes/systemli/assets/js/main.js
+++ themes/systemli/assets/js/main.js
@@ -1,57 +1,59 @@
 (function () {
+  // show random sticker on homepage
+  const sticker = document.querySelectorAll(".sticker");
+  if (sticker.length > 0) {
+    const randomSticker = sticker.item(Math.floor(Math.random() * sticker.length));
+    randomSticker.src = randomSticker.dataset.src;
+    randomSticker.classList.remove("hidden");
+  }
 
-    // show random sticker on homepage
-    const sticker = document.querySelectorAll('.sticker');
-    if (sticker.length > 0) {
-        const randomSticker = sticker.item(Math.floor(Math.random() * sticker.length));
-        randomSticker.src = randomSticker.dataset.src;
-        randomSticker.classList.remove('hidden');
-    }
+  // show and hide mobile menu
+  function openMobileMenu() {
+    document.getElementById("navbar").style.transform = "translateX(0px)";
+    document.getElementById("mobile-menu-overlay").classList.remove("hidden");
+  }
+  function closeMobileMenu() {
+    document.getElementById("navbar").style.transform = "translateX(375px)";
+    document.getElementById("mobile-menu-overlay").classList.add("hidden");
+  }
+  document.getElementById("button-menu-open").addEventListener("click", function () {
+    openMobileMenu();
+  });
+  document.getElementById("button-menu-close").addEventListener("click", function () {
+    closeMobileMenu();
+  });
+  document.getElementById("mobile-menu-overlay").addEventListener("click", function () {
+    closeMobileMenu();
+  });
 
-    // show and hide mobile menu
-    function openMobileMenu() {
-        document.getElementById('navbar').style.transform = 'translateX(0px)';
-        document.getElementById('mobile-menu-overlay').classList.remove('hidden');
-    }
-    function closeMobileMenu() {
-        document.getElementById('navbar').style.transform = 'translateX(375px)';
-        document.getElementById('mobile-menu-overlay').classList.add('hidden');
-    }
-    document.getElementById('button-menu-open').addEventListener('click', function () {
-        openMobileMenu();
+  // show service description on hover
+  const serviceBarLinks = document.getElementsByClassName("service-bar-link");
+  if (serviceBarLinks.length > 0) {
+    Array.prototype.forEach.call(serviceBarLinks, function (service) {
+      service.addEventListener("mouseover", function (event) {
+        const id = event.currentTarget
+          .getElementsByClassName("service-title-container")[0]
+          .getElementsByClassName("service-title")[0].innerText;
+        document.getElementById(id).style.display = "flex";
+      });
+      service.addEventListener("mouseout", function (event) {
+        const id = event.currentTarget
+          .getElementsByClassName("service-title-container")[0]
+          .getElementsByClassName("service-title")[0].innerText;
+        document.getElementById(id).style.display = "none";
+      });
     });
-    document.getElementById('button-menu-close').addEventListener('click', function () {
-        closeMobileMenu();
+  }
+  // hover effect on service tiles
+  const serviceTiles = document.getElementsByClassName("service-tile");
+  if (serviceTiles.length > 0) {
+    Array.prototype.forEach.call(serviceTiles, function (tile) {
+      tile.addEventListener("mouseover", function (event) {
+        tile.getElementsByClassName("service-tile-upper")[0].classList.add("border-pink");
+      });
+      tile.addEventListener("mouseout", function (event) {
+        tile.getElementsByClassName("service-tile-upper")[0].classList.remove("border-pink");
+      });
     });
-    document.getElementById('mobile-menu-overlay').addEventListener('click', function () {
-        closeMobileMenu();
-    });
-
-    // show service description on hover
-    const serviceBarLinks = document.getElementsByClassName('service-bar-link');
-    if (serviceBarLinks.length > 0) {
-        Array.prototype.forEach.call(serviceBarLinks, function (service) {
-            service.addEventListener('mouseover', function (event) {
-                const id = event.currentTarget.getElementsByClassName('service-title-container')[0].getElementsByClassName('service-title')[0].innerText;
-                document.getElementById(id).style.display = 'flex';
-            });
-            service.addEventListener('mouseout', function (event) {
-                const id = event.currentTarget.getElementsByClassName('service-title-container')[0].getElementsByClassName('service-title')[0].innerText;
-                document.getElementById(id).style.display = 'none';
-            });
-        });
-
-    }
-    // hover effect on service tiles
-    const serviceTiles = document.getElementsByClassName('service-tile');
-    if (serviceTiles.length > 0) {
-        Array.prototype.forEach.call(serviceTiles, function (tile) {
-            tile.addEventListener('mouseover', function (event) {
-                tile.getElementsByClassName('service-tile-upper')[0].classList.add('border-pink');
-            });
-            tile.addEventListener('mouseout', function (event) {
-                tile.getElementsByClassName('service-tile-upper')[0].classList.remove('border-pink');
-            });
-        })
-    }
+  }
 })();
--- themes/systemli/assets/js/matomo.js
+++ themes/systemli/assets/js/matomo.js
@@ -1,9 +1,9 @@
-var _paq = window._paq = window._paq || []
+var _paq = (window._paq = window._paq || []);
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(['setCookieDomain', '*.systemli.org'])
-_paq.push(['setDoNotTrack', true])
-_paq.push(['disableCookies'])
-_paq.push(['trackPageView'])
-_paq.push(['enableLinkTracking'])
-_paq.push(['setTrackerUrl', 'https://analytics.systemli.org/matomo.php'])
-_paq.push(['setSiteId', '2'])
+_paq.push(["setCookieDomain", "*.systemli.org"]);
+_paq.push(["setDoNotTrack", true]);
+_paq.push(["disableCookies"]);
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+_paq.push(["setTrackerUrl", "https://analytics.systemli.org/matomo.php"]);
+_paq.push(["setSiteId", "2"]);
